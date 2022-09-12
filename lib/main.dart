c6854d1523d9af09ef750f05aeba7aeccf8b1540
Fa81J <88445817+Fa81J@users.noreply.github.com>
2022-09-12 19:52:37 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import  'package:untitled3/homeController/homeController.dart';
+import 'package:untitled5/homeController/homeController.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Remove Background App',
       home: HomeScreen(),
     );
@@ -21,36 +24,57 @@ class MyApp extends StatelessWidget {
 
 class HomeScreen extends GetView<HomeController> {
 
- //HomeController controller=Get.put(HomeController());
+//HomeController controller=Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffD9D9D9) ,
       appBar: AppBar(
-        backgroundColor: Colors.black12,
-        title: Text("Slazzer Ai App"),
+        backgroundColor: Color(0xff4F7E9E),
+        title: Text("Slazzer Ai App",
+        style: TextStyle(color: Color(0xffD9D9D9))
+        ),
       ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(onPressed: () {}, child: Text('pick image')),
+              ElevatedButton(onPressed: () {
+                controller.getImage();
+                 },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff4F7E9E),
+                  ),
+                  child: Text('pick image',
+                  style: TextStyle(color: Color(0xffD9D9D9))
+              )),
            GetBuilder<HomeController>(
                init: HomeController(),
                builder: (controller){
           return Center(child: controller.imageBefore == null
-                  ? Text("plz pick image"):
+                  ? Text("please pick image",
+              style: TextStyle(color: Color(0xff4F7E9E))
+          ):
                 Image.file(controller.imageBefore!),
                 );
               }),
+              SizedBox(height: 12),
               ElevatedButton(onPressed: () {
                 controller.uploadImage(controller.imageBefore!);
-              }, child: Text('upload image')),
+              },   style: ElevatedButton.styleFrom(
+                primary: Color(0xff4F7E9E),
+              ),
+                  child: Text('upload image',
+                  style: TextStyle(color: Color(0xffD9D9D9))
+              )),
               GetBuilder<HomeController>(builder: (controller) {
                 return Center(child: controller.imageBefore == null
-                    ? Text("plz upload image") :
-                Image.memory(controller.imageAfter!)
+                    ? Text("please upload image",
+                    style: TextStyle(color: Color(0xff4F7E9E))
+                ) :
+                 Image.memory(controller.imageAfter!)
                 );
               })
                 ],
